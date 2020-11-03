8b3691f7dc34311f0f6e17c8db37ad7763c86787
Aline Freitas <aline.freitas@escale.com.br>
2020-11-03 11:01:09 -0300

    docs: add documentation for mysql module

--- modules/mysql/variables.tf
+++ modules/mysql/variables.tf
@@ -1,6 +1,17 @@
+# Database host
 variable "db_host" { type = "string" }
+
+# Database name to be created
 variable "db_name" { type = "string" }
+
+# master username with database/user creation permission
 variable "db_master" { type = "string" }
+
+# master password for user with database/user creation permission
 variable "db_master_password" { type = "string" }
+
+# New user to be created
 variable "db_username" { type = "string" }
+
+# Password for new user to be created
 variable "db_password" { type = "string" }
--- modules/mysql/versions.tf
+++ modules/mysql/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     mysql = {
-      source = "terraform-providers/mysql"
+      source  = "terraform-providers/mysql"
+      version = "~> 1.6"
     }
   }
   required_version = ">= 0.13"
