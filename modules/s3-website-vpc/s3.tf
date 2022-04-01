697cd272b27bfcb22c659bc7a00ab55469531bd3
Aline Freitas <aline.freitas@escale.com.br>
2022-04-01 12:33:53 -0300

    fix: remove aws policy left after refactoring (#16)
    * fix: remove aws policy left after refactoring
    
    * refactor: upgrade circleci image to next-gen

--- modules/s3-website-vpc/s3.tf
+++ modules/s3-website-vpc/s3.tf
@@ -4,53 +4,77 @@ resource "aws_s3_bucket" "internal-logs" {
   tags   = var.tags
 }
 
+resource "aws_s3_bucket_acl" "internal-logs" {
+  bucket = aws_s3_bucket.internal-logs.bucket
+  acl    = "log-delivery-write"
+}
+
 resource "aws_s3_bucket" "internal" {
   bucket = var.domain
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_acl" "internal" {
+  bucket = aws_s3_bucket.internal.bucket
   acl    = "private"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "internal" {
+  bucket = aws_s3_bucket.internal.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 
-  logging {
-    target_bucket = aws_s3_bucket.internal-logs.id
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_logging" "internal" {
+  bucket        = aws_s3_bucket.internal.bucket
+  target_bucket = aws_s3_bucket.internal-logs.id
+}
+
+resource "aws_s3_bucket_policy" "internal" {
+  bucket = aws_s3_bucket.internal.bucket
+  policy = data.aws_iam_policy_document.internal
+}
+
+data "aws_iam_policy_document" "internal" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    sid       = "PublicReadVpce"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_website.arn}/*"]
+    effect    = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceVpce"
+      values   = [aws_vpc_endpoint.internal.id]
+    }
   }
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "${title(var.app_name)}InternalTestPolicy"
-    Statement = [
-      {
-        Sid       = "PublicReadVpce"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource = [
-          "arn:aws:s3:::${var.domain}/*"
-        ]
-        Condition = {
-          StringEquals = {
-            "aws:SourceVpce" = aws_vpc_endpoint.internal.id
-          }
-        }
-      },
-      {
-        Sid       = "DenyPublicReadNonVpce"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource = [
-          "arn:aws:s3:::${var.domain}/*"
-        ]
-        Condition = {
-          StringNotEquals = {
-            "aws:SourceVpce" = aws_vpc_endpoint.internal.id
-          }
-        }
-      }
-    ]
-  })
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
 
-  tags = var.tags
+    sid       = "DenyPublicReadNonVpce"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_website.arn}/*"]
+    effect    = "Deny"
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpce"
+      values   = [aws_vpc_endpoint.internal.id]
+    }
+  }
 }
