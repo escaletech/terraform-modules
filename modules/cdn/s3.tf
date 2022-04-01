dd814c2e1fdbf745c0be08bc704f0833e1368ff2
Aline Freitas <aline.freitas@escale.com.br>
2022-04-01 12:12:36 -0300

    refactor: refactoring s3 resource to aws provider v4 (#14)

--- modules/cdn/s3.tf
+++ modules/cdn/s3.tf
@@ -3,3 +3,8 @@ resource "aws_s3_bucket" "cloudfront-logs" {
   acl    = "private"
   tags   = var.tags
 }
+
+resource "aws_s3_bucket_acl" "cloudfront-logs" {
+  bucket = aws_s3_bucket.cloudfront-logs.bucket
+  acl    = "private"
+}
