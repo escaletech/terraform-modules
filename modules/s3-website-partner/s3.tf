08c70c505d1b5d51d4419aa3c9e10947aa5f01b9
Aline Freitas <aline.freitas@escale.com.br>
2022-04-01 17:32:33 -0300

    fix: additional fix for s3 new format in aws v4

--- modules/s3-website-partner/s3.tf
+++ modules/s3-website-partner/s3.tf
@@ -10,51 +10,69 @@ resource "aws_s3_bucket_acl" "internal-logs" {
 
 resource "aws_s3_bucket" "internal" {
   bucket = var.domain
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_acl" "internal" {
+  bucket = aws_s3_bucket.internal.bucket
   acl    = "private"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "internal" {
+  bucket = aws_s3_bucket.internal.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 
-  logging {
-    target_bucket = aws_s3_bucket.internal-logs.id
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_logging" "internal" {
+  bucket = aws_s3_bucket.internal.id
+
+  target_bucket = aws_s3_bucket.internal-logs.id
+  target_prefix = "log/"
+}
+
+resource "aws_s3_bucket_policy" "internal" {
+  bucket = aws_s3_bucket.internal.id
+  policy = data.aws_iam_policy_document.internal.json
+}
+
+data "aws_iam_policy_document" "internal" {
+  policy_id = "${title(var.app_name)}InternalTestPolicy"
+  statement {
+    sid    = "PublicReadVpce"
+    effect = "Allow"
+    principal {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.internal.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceVpce"
+      values   = [aws_vpc_endpoint.internal.id]
+    }
   }
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "${title(var.app_name)}InternalTestPolicy"
-    Statement = [
-      {
-        Sid       = "PublicReadVpce"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource = [
-          "arn:aws:s3:::${var.domain}/*"
-        ]
-        Condition = {
-          StringEquals = {
-            "aws:SourceVpce" = aws_vpc_endpoint.internal.id
-          }
-        }
-      },
-      {
-        Sid       = "DenyPublicReadNonVpce"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource = [
-          "arn:aws:s3:::${var.domain}/*"
-        ]
-        Condition = {
-          StringNotEquals = {
-            "aws:SourceVpce" = aws_vpc_endpoint.internal.id
-          }
-        }
-      }
-    ]
-  })
-
-  tags = var.tags
+  statement {
+    sid    = "DenyPublicReadNonVpce"
+    effect = "Deny"
+    principal {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.internal.arn}/*"]
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpce"
+      values   = [aws_vpc_endpoint.internal.id]
+    }
+  }
 }
