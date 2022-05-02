28b52289bef017f41aec53bb6332593f13f5ba64
lguerrero-1807 <lucas.1807@hotmail.com>
2022-05-02 17:39:18 -0300

    fix: fixing kubectl blocks

--- modules/k8s-deploy/provider.tf
+++ modules/k8s-deploy/provider.tf
@@ -24,14 +24,12 @@ provider "helm" {
 }
 
 provider "kubectl" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.name]
-      command     = "./aws/dist/aws"
-    }
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.name]
+    command     = "./aws/dist/aws"
   }
 }
 
