96bd1df960c3f4f36496ee9b6f5d1f4dc53429bb
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2022-05-02 17:30:20 -0300

    fix: fixing provider, adding kubectl provider (#49)

--- modules/k8s-deploy/provider.tf
+++ modules/k8s-deploy/provider.tf
@@ -23,6 +23,18 @@ provider "helm" {
   }
 }
 
+provider "kubectl" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.name]
+      command     = "./aws/dist/aws"
+    }
+  }
+}
+
 module "kube-system" {
   source          = "./kube-system"
   cluster-name    = var.cluster-name
