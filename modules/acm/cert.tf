dd0b62d176c3755c791f0d9d71dab10d37aafdee
Aline Freitas <aline.freitas@escale.com.br>
2022-05-18 10:26:12 -0300

    feat: create acm module

---
+++ modules/acm/cert.tf
@@ -0,0 +1,33 @@
+data "aws_route53_zone" "zone" {
+  name = var.dns_zone
+}
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.certificate_host
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "private-ingress" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
---
+++ modules/acm/variables.tf
@@ -0,0 +1,7 @@
+variable "dns_zone" {
+  type = string
+}
+
+variable "certificate_host" {
+  type = string
+}
