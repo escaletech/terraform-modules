66a542850cfa21b4819781d5018ec90b6c89623b
lguerrero-1807 <lucas.1807@hotmail.com>
2022-06-15 11:32:19 -0300

    fix: fixing alternative domain variable

--- modules/acm-multiple-domains/main.tf
+++ modules/acm-multiple-domains/main.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "certificate" {
   domain_name               = var.certificate_host
   validation_method         = "DNS"
-  subject_alternative_names = [var.alternative_domain]
+  subject_alternative_names = var.alternative_domain
 
   lifecycle {
     create_before_destroy = true
@@ -9,8 +9,8 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 module "acm-multiple-domains" {
-  for_each = {for domain in aws_acm_certificate.certificate.domain_validation_options: domain.domain_name => domain}
-  source  = "./terraform-aws-acm-multiple-domains"
+  for_each        = { for domain in aws_acm_certificate.certificate.domain_validation_options : domain.domain_name => domain }
+  source          = "./terraform-aws-acm-multiple-domains"
   certificate_arn = aws_acm_certificate.certificate.arn
   domain          = each.key
   name            = each.value.resource_record_name
@@ -21,5 +21,5 @@ module "acm-multiple-domains" {
 
 resource "aws_acm_certificate_validation" "validate" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for domain in module.acm-multiple-domains : domain.record.fqdn ]
+  validation_record_fqdns = [for domain in module.acm-multiple-domains : domain.record.fqdn]
 }
