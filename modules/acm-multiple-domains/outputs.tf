4eb3c5b01655024a2ae1a90c941fec40532dd0ee
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2022-06-15 11:46:22 -0300

    fix: deleting unnecessary files (#72)
    * fix: deleting unnecessary files
    
    * fix: adding necessary variable
    
    * fix: terraform fmt

--- modules/acm-multiple-domains/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "record" {
-  value       = aws_route53_record.tls-entry
-  sensitive   = false
-  description = "DNS entry for certificate manager authentication."
-}
--- modules/acm-multiple-domains/variables.tf
+++ modules/acm-multiple-domains/variables.tf
@@ -1,33 +1,7 @@
-variable "certificate_arn" {
-  description = "Certificate ARN."
-}
-variable "domain" {
-  description = "Domain name to authenticate against."
-}
-variable "name" {
-  description = "Name of local DNS record."
-}
-variable "type" {
-  description = "DNS record type."
-}
-variable "record" {
-  description = "Certificate Manager DNS entry."
-}
-variable "ttl" {
-  description = "TTL for DNS record."
-}
 variable "certificate_host" {
   type = string
 }
+
 variable "alternative_domain" {
   type = list(string)
-}
-
-locals {
-  certificate_arn = var.certificate_arn
-  domain          = replace(var.domain, "*.", "")
-  name            = var.name
-  type            = var.type
-  record          = var.record
-  ttl             = var.ttl
 }
