42c30d83536e3912471392bc11d69d6fc784d1ee
lguerrero-1807 <lucas.1807@hotmail.com>
2022-06-15 10:35:25 -0300

    fix: fixing syntax

--- modules/acm-multiple-domains/main.tf
+++ modules/acm-multiple-domains/main.tf
@@ -15,14 +15,14 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_route53_record" "tls-entry" {
   for_each = {
-  for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
-    type   = dvo.resource_record_type
-    record = dvo.resource_record_value
-  }
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
   }
 
-  zone_id = data.aws_route53_zone.zone.zone_id
+  zone_id = data.aws_route53_zone.domain.zone_id
   name    = each.value.name
   type    = each.value.type
   records = [each.value.record]
