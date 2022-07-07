111dbf240e32d9d4be5f4926c1e8d1e0c07fd112
lguerrero-1807 <lucas.1807@hotmail.com>
2022-07-07 17:03:34 -0300

    fix: Fixing variables

--- modules/s3-website-vpc-endpoint/variables.tf
+++ modules/s3-website-vpc-endpoint/variables.tf
@@ -1,13 +1,6 @@
-variable "app_name" { type = string }
-variable "domain" { type = string }
-variable "dns_zone_name" { type = string }
 variable "subnets" { type = list(any) }
 variable "tags" { type = map(any) }
 variable "vpc_name" { type = string }
-variable "private_zone" {
-  type    = bool
-  default = false
-}
 
 variable "internal_ip" {
   type    = bool
@@ -21,9 +14,4 @@ data "aws_vpc" "vpc" {
     name   = "tag:Name"
     values = [var.vpc_name]
   }
-}
-
-data "aws_route53_zone" "zone" {
-  name         = var.dns_zone_name
-  private_zone = var.private_zone
 }
