387c4a953d145010c7fad16a4a5f4d5ec0db71f8
lguerrero-1807 <lucas.1807@hotmail.com>
2022-07-07 17:18:21 -0300

    fix: removing internal ip options

--- modules/s3-website-vpc-endpoint/variables.tf
+++ modules/s3-website-vpc-endpoint/variables.tf
@@ -1,12 +1,6 @@
 variable "subnets" { type = list(any) }
 variable "tags" { type = map(any) }
 variable "vpc_name" { type = string }
-
-variable "internal_ip" {
-  type    = bool
-  default = true
-}
-
 data "aws_region" "current" {}
 
 data "aws_vpc" "vpc" {
