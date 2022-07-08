7eabf05b50bfcf6c53bb1ed7fe512a070f2550d8
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2022-07-08 16:37:25 -0300

    Feat/s3 website route53 (#93)
    * feat: creating new module to s3-website-route53
    
    * feat: creating new module to s3-website-route53
    
    * fix: terraform fmt

---
+++ modules/s3-website-route53/data.tf
@@ -0,0 +1,18 @@
+data "aws_vpc" "escale-staging" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc-name]
+  }
+}
+
+data "aws_vpc_endpoint" "s3" {
+  vpc_id = data.aws_vpc.escale-staging.id
+  filter {
+    name   = "tag:Name"
+    values = [var.endpoint-name]
+  }
+}
+
+data "aws_lb" "alb_staging" {
+  name = "vpc-endpoint-websites-s3"
+}
