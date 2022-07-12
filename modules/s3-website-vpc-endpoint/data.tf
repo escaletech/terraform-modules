bd1276e934e4514d7b5bee0b1aa985a4b7e298de
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2022-07-12 17:49:48 -0300

    fix: changing s3-website-vpc-endpoint to a multi-domain module (#99)
    * fix: changing s3-website-vpc-endpoint to a multi-domain module
    
    * fix: adding for_each on data.tf

--- modules/s3-website-vpc-endpoint/data.tf
+++ modules/s3-website-vpc-endpoint/data.tf
@@ -1,5 +1,15 @@
-data "aws_acm_certificate" "escale-staging" {
-  domain      = "*.staging.escale.com.br"
+data "aws_region" "current" {}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_acm_certificate" "domains" {
+  for_each    = var.domains
+  domain      = each.value
   types       = ["AMAZON_ISSUED"]
   most_recent = true
 }
