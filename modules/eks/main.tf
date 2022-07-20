4e8bda9ebd86b35c87c4be290c4a7b0db207f319
Aline Freitas <aline.freitas@escale.com.br>
2022-07-20 10:47:27 -0300

    refactor: tag eks instances

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -200,6 +200,28 @@ resource "aws_eks_node_group" "node_group_private" {
   }
 }
 
+resource "aws_autoscaling_group_tag" "nodes_group" {
+  for_each = toset(
+    [for asg in flatten(
+      [for resources in aws_eks_node_group.nodes_group.resources : resources.autoscaling_groups]
+    ) : asg.name]
+  )
+
+  autoscaling_group_name = each.value
+
+  tag {
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Owner"
+    value               = "Engineering"
+    propagate_at_launch = true
+  }
+}
+
 ##############
 # EKS Addons #
 ##############
