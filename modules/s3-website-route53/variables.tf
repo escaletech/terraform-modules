f65e2324c743dbba494272cf5309d250e5e56e86
lguerrero-1807 <lucas.1807@hotmail.com>
2022-08-03 10:38:23 -0300

    feat: Adding tags parameters on s3-website-route-53

--- modules/s3-website-route53/variables.tf
+++ modules/s3-website-route53/variables.tf
@@ -1,4 +1,19 @@
-variable "tags" { type = map(any) }
+variable "tags" {
+  type    = map(any)
+  default = {}
+}
+
+resource "null_resource" "check-tags" {
+  lifecycle {
+    precondition {
+      condition     = length(var.tags) != 0 || length(data.aws_default_tags.test.tags) != 0
+      error_message = "Tags are required!"
+    }
+  }
+}
+
+
+
 variable "domain" {
   type = string
 }
