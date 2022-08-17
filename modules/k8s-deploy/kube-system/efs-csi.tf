62276162f5c6638a10119c0883d6173f4993f2d3
Aline Freitas <aline.freitas@escale.com.br>
2022-08-17 11:08:50 -0300

    fix: argument is security groups (plural)

--- modules/k8s-deploy/kube-system/efs-csi.tf
+++ modules/k8s-deploy/kube-system/efs-csi.tf
@@ -91,8 +91,8 @@ resource "aws_efs_file_system" "efs-eks" {
 }
 
 resource "aws_efs_mount_target" "efs-eks" {
-  count          = var.efs-enabled != true ? 0 : length(data.aws_subnets.main.ids)
-  file_system_id = aws_efs_file_system.efs-eks[0].id
-  subnet_id      = data.aws_subnets.main.ids[count.index]
-  security_group = [aws_security_group.allow_efs.id]
+  count           = var.efs-enabled != true ? 0 : length(data.aws_subnets.main.ids)
+  file_system_id  = aws_efs_file_system.efs-eks[0].id
+  subnet_id       = data.aws_subnets.main.ids[count.index]
+  security_groups = [aws_security_group.allow_efs.id]
 }
