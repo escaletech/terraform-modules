117b48ea0e1516bc06097df246bdff4e8839e35f
lguerrero-1807 <lucas.1807@hotmail.com>
2022-11-30 15:27:21 -0300

    fix: fixing resource name

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -243,14 +243,14 @@ resource "aws_eks_addon" "core_dns" {
   )
 }
 
-resource "aws_eks_addon" "core_dns" {
+resource "aws_eks_addon" "vpc-cni" {
   cluster_name      = aws_eks_cluster.cluster.name
   addon_name        = "vpc-cni"
-  addon_version     = "1.11.4-eksbuild.1"
+  addon_version     = "v1.11.4-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
   tags = merge(var.tags, {
     "eks_addon" = "vpc-cni"
-  }
+    }
   )
 }
 
