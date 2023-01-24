822a5405a2053761318dd0f380526184e94c7b29
Murillo G Zampieri <murillo.zampieri@escale.com.br>
2023-01-24 14:02:20 -0300

    chore: add policy to gateway

--- modules/api-gateway/gateway.tf
+++ modules/api-gateway/gateway.tf
@@ -7,3 +7,23 @@ resource "aws_api_gateway_rest_api" "gateway_api" {
   name                         = local.name
   disable_execute_api_endpoint = true
 }
+
+resource "aws_api_gateway_rest_api_policy" "policy_invoke" {
+  rest_api_id = aws_api_gateway_rest_api.gateway_api.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "execute-api:Invoke",
+      "Resource": "${aws_api_gateway_rest_api.gateway_api.execution_arn}"
+    }
+  ]
+}
+EOF
+}
