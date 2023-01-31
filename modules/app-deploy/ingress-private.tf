06387b9bfe3b36cc16727cc2eb34939b19bbeb1a
Murillo G Zampieri <murillo.zampieri@escale.com.br>
2023-01-31 13:39:23 -0300

    chore: rename ingress private

--- modules/app-deploy/ingress-private.tf
+++ modules/app-deploy/ingress-private.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_ingress_v1" "private" {
   count = var.host-private != null ? 1 : 0
   metadata {
-    name      = var.app-name
+    name      = "${var.app-name}-private"
     namespace = var.namespace
     labels = {
       app = var.app-name
