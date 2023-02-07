50d829bbbaaeaa4813cd7b33e6912c64b4cb5d3b
lguerrero-1807 <lucas.1807@hotmail.com>
2023-02-07 15:45:43 -0300

    feat: adding disk_size on eks node group

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -181,6 +181,7 @@ resource "aws_eks_node_group" "node_group_private" {
   node_role_arn   = aws_iam_role.nodes.arn
   subnet_ids      = [var.private_subnet_ids[count.index]]
   instance_types  = [var.node-instance-type]
+#  disk_size       = 60
 
   scaling_config {
     desired_size = var.asg-desired-capacity
