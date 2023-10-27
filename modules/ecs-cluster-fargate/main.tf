748ca8eaa6ee29db9f723241a0692fae8970dcd2
lguerrero-1807 <lucas.1807@hotmail.com>
2023-10-27 16:59:14 -0300

    feat: adding new fields on ecs-fargate-module

--- modules/ecs-cluster-fargate/main.tf
+++ modules/ecs-cluster-fargate/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecs_cluster" "ecs-cluster-fargate" {
-  name = var.cluster_name
+  depends_on = [aws_service_discovery_http_namespace.default-ecs-cluster-fargate]
+  name       = var.cluster_name
 
   capacity_providers = ["FARGATE"]
   default_capacity_provider_strategy {
@@ -12,7 +13,21 @@ resource "aws_ecs_cluster" "ecs-cluster-fargate" {
     value = var.enable_container_insights
   }
 
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+  service_connect_defaults = {
+    namespace = aws_service_discovery_http_namespace.default-ecs-cluster-fargate.arn
+  }
+
   tags = var.tags
+}
+
+resource "aws_service_discovery_http_namespace" "default-ecs-cluster-fargate" {
+  name        = var.cluster_name
+  description = "Default namespace"
 }
 
 resource "aws_iam_role" "ecs_execution_role" {
