db50a8d8ee787f9abc51534b5348fee7dd91e678
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2023-10-30 18:41:18 -0300

    feat: first setup to ecs-task module (#183)
    * feat: first setup to ecs-task module
    
    * fix: changing few names

---
+++ modules/ecs-task/iam.tf
@@ -0,0 +1,65 @@
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role-${var.family}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecs_task_cloudwatch_logs_policy" {
+  name        = "ecs_task_cloudwatch_logs_policy"
+  description = "Política para permitir logs do CloudWatch"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "logs:CreateLogGroup",
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "attach_ecsTaskExecutionRole" {
+  name       = "attach-ecsTaskExecutionRole"
+  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_policy_attachment" "attach_ecrReadOnly" {
+  name       = "attach-AmazonElasticContainerRegistryPublicReadOnly"
+  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicReadOnly"
+}
+
+
+resource "aws_iam_policy_attachment" "attach_SecretsManagerReadWrite" {
+  name       = "SecretsManagerReadWrite"
+  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_policy_attachment" "attach_ecs_task_cloudwatch_logs_policy" {
+  depends_on = [aws_iam_policy.ecs_task_cloudwatch_logs_policy]
+  name       = "ecs_task_cloudwatch_logs_policy"
+  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  policy_arn = "arn:aws:iam::aws:policy/ecs_task_cloudwatch_logs_policy"
+}
