296c1d995598c142546a4ed852765879b1aaaf4a
lguerrero-1807 <lucas.1807@hotmail.com>
2023-10-30 19:10:11 -0300

    fix: fixing roles names

--- modules/ecs-task/iam.tf
+++ modules/ecs-task/iam.tf
@@ -17,25 +17,25 @@ resource "aws_iam_role" "ecs_task_role" {
 
 resource "aws_iam_policy_attachment" "attach_ecsTaskExecutionRole" {
   name       = "attach-ecsTaskExecutionRole"
-  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  roles      = ["aws_iam_role.ecs-task-role-${var.family}.name"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_policy_attachment" "attach_ecrReadOnly" {
   name       = "attach-AmazonElasticContainerRegistryPublicReadOnly"
-  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  roles      = ["aws_iam_role.ecs-task-role-${var.family}.name"]
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticContainerRegistryPublicReadOnly"
 }
 
 
 resource "aws_iam_policy_attachment" "attach_SecretsManagerReadWrite" {
   name       = "SecretsManagerReadWrite"
-  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  roles      = ["aws_iam_role.ecs-task-role-${var.family}.name"]
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
 resource "aws_iam_policy_attachment" "attach_ecs_task_cloudwatch_logs_policy" {
   name       = "ecs_task_cloudwatch_logs_policy"
-  roles      = ["aws_iam_role.ecs_task_role_${var.family}.name"]
+  roles      = ["aws_iam_role.ecs-task-role-${var.family}.name"]
   policy_arn = data.aws_iam_policy.ecs_task_cloudwatch_logs_policy.arn
 }
