f7fd94e3d44f1166b47201e69a2046859e42c823
Lucas Guerrero <34523718+lguerrero-1807@users.noreply.github.com>
2023-11-21 14:15:12 -0300

    fix: cleaning ecs module fargate (#186)

--- modules/ecs-cluster-fargate/main.tf
+++ modules/ecs-cluster-fargate/main.tf
@@ -29,23 +29,3 @@ resource "aws_service_discovery_http_namespace" "default-ecs-cluster-fargate" {
   name        = var.cluster_name
   description = "Default namespace"
 }
-
-#resource "aws_iam_role" "ecs_execution_role" {
-#  name = "ecsTaskExecutionRole"
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [{
-#      Action = "sts:AssumeRole",
-#      Effect = "Allow",
-#      Principal = {
-#        Service = "ecs-tasks.amazonaws.com"
-#      }
-#    }]
-#  })
-#}
-
-#resource "aws_iam_policy_attachment" "ecs_execution_policy_attachment" {
-#  name       = "ecs-execution-policy-attachment"
-#  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-#  roles      = data.aws_iam_role.ecstaskexecutinonrole.name
-#}
