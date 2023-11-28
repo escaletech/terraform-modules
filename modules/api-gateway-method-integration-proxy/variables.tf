cac2a03516b8ae68616d8ad96141db7db3a32ac3
Murillo G. Zampieri <murillo.zampieri@escale.com.br>
2023-11-28 13:43:33 -0300

    feat: add module to proxy integration request

---
+++ modules/api-gateway-method-integration-proxy/variables.tf
@@ -0,0 +1,37 @@
+variable "api_gateway_id" {
+  description = "API Gateway's ID"
+  type        = string
+}
+
+variable "resource_id" {
+  description = "Resource's ID"
+  type        = string
+}
+
+variable "method" {
+  description = "Method"
+  type        = string
+  default     = "ANY"
+}
+
+variable "authorization" {
+  description = "Authorization"
+  type        = string
+  default     = "NONE"
+}
+
+variable "authorizer_id" {
+  description = "Authorizer's ID"
+  type        = string
+  default     = null
+}
+
+variable "uri_origin" {
+  description = "URI Origin"
+  type        = string
+}
+
+variable "vpc_link_id" {
+  description = "VPC Link's ID"
+  type        = string
+}
