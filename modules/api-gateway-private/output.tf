405dd3bb0bed71a7bb1b9245fb4cfc1bd43c2713
Murillo G. Zampieri <murillo.zampieri@escale.com.br>
2023-12-29 17:55:43 -0300

    feat add output arn to gateway private

--- modules/api-gateway-private/output.tf
+++ modules/api-gateway-private/output.tf
@@ -5,3 +5,8 @@ output "id" {
 output "root_resource_api_id" {
   value = aws_api_gateway_rest_api.gateway_api.root_resource_id
 }
+
+
+output "gateway_api_arn" {
+  value = aws_api_gateway_rest_api.gateway_api.arn
+}
