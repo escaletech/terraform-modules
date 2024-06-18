5e7977bfc067d3e289c0352d7cda5c3c4d8eeb57
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2024-06-18 17:34:57 -0300

    feat: change var allow_overwrite in main

--- modules/cdn/dns.tf
+++ modules/cdn/dns.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "main" {
   zone_id         = data.aws_route53_zone.zone.zone_id
   name            = var.host
   type            = "A"
-  allow_overwrite = true
+  allow_overwrite = var.allow_overwrite
 
   alias {
     name    = (var.alias_main != null) ? var.alias_main : aws_cloudfront_distribution.main.domain_name
