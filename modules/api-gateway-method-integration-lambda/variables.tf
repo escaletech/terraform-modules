948bf3a908266e1d6d565d1f863536e684fa93ed
anselmojoao <169174630+anselmojoao@users.noreply.github.com>
2024-07-26 18:13:00 -0300

    feat: add integration lambda (#219)
    * feat: add integration lambda
    
    * feat: remove vpc_link
    
    * feat: remove vpc_link
    
    * feat: change type
    
    * feat: add parameter
    
    * feat: change type
    
    * feat: add requests
    
    ---------
    
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

---
+++ modules/api-gateway-method-integration-lambda/variables.tf
@@ -0,0 +1,48 @@
+variable "api_gateway_id" {
+  description = "API Gateway's ID"
+  type        = string
+}
+
+variable "resource_id" {
+  description = "Resource's ID"
+  type        = string
+}
+
+variable "method" {
+  description = "Method"
+  type        = string
+  default     = "ANY"
+}
+
+variable "authorization" {
+  description = "Authorization"
+  type        = string
+  default     = "NONE"
+}
+
+variable "authorizer_id" {
+  description = "Authorizer's ID"
+  type        = string
+  default     = null
+}
+
+variable "uri_origin" {
+  description = "URI Origin"
+  type        = string
+}
+
+variable "request_models" {
+  description = "Method's Request Models"
+  type        = map(any)
+  default = {
+   "application/json" = "OperationRequired"
+  }
+}
+
+variable "request_parameters_integration" {
+  description = "Request Parameters"
+  type        = map(any)
+  default = {
+    "method.request.header.Instance" = true
+  }
+}
