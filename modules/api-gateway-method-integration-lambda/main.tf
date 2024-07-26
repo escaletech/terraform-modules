a62cd07c2719d89a5d5862c9903946591c4bc8b9
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2024-07-26 13:25:06 -0300

    feat: change type

--- modules/api-gateway-method-integration-lambda/main.tf
+++ modules/api-gateway-method-integration-lambda/main.tf
@@ -25,62 +25,62 @@ resource "aws_api_gateway_integration" "lambda" {
 
   request_templates = {
     "application/json" = <<EOF
-    #set( $body = $input.json("$") )
+#set( $body = $input.json("$") )
 
-    #define( $loop )
-      {
-      #foreach($key in $map.keySet())
-          #set( $k = $util.escapeJavaScript($key) )
-          #set( $v = $util.escapeJavaScript($map.get($key)).replaceAll("\\'", "'") )
-          "$k":
-            "$v"
-            #if( $foreach.hasNext ) , #end
-      #end
-      }
+#define( $loop )
+  {
+  #foreach($key in $map.keySet())
+      #set( $k = $util.escapeJavaScript($key) )
+      #set( $v = $util.escapeJavaScript($map.get($key)).replaceAll("\\'", "'") )
+      "$k":
+        "$v"
+        #if( $foreach.hasNext ) , #end
+  #end
+  }
+#end
+
+{
+  "body": "$util.escapeJavaScript($input.json('$'))",
+  "method": "$context.httpMethod",
+  "principalId": "$context.authorizer.principalId",
+  "stage": "$context.stage",
+  "headers": {
+    "Apigtw-Request-Id" : "$context.requestId",
+    "Escale-User-Id": "$context.authorizer.principalId",
+    "Partner": "$context.authorizer.brand",
+    "Product": "$context.authorizer.product",
+    #foreach($param in $input.params().header.keySet())
+    "$param": "$util.escapeJavaScript($input.params().header.get($param))"
+    #if($foreach.hasNext),#end
     #end
+  },
 
-    {
-      "body": "$util.escapeJavaScript($input.json('$'))",
-      "method": "$context.httpMethod",
-      "principalId": "$context.authorizer.principalId",
-      "stage": "$context.stage",
-      "headers": {
-        "Apigtw-Request-Id" : "$context.requestId",
-        "Escale-User-Id": "$context.authorizer.principalId",
-        "Partner": "$context.authorizer.brand",
-        "Product": "$context.authorizer.product",
-        #foreach($param in $input.params().header.keySet())
-        "$param": "$util.escapeJavaScript($input.params().header.get($param))"
-        #if($foreach.hasNext),#end
-        #end
-      },
+  "cognitoPoolClaims" : {
+    
+    "sub": "$context.authorizer.claims.sub"
+  },
 
-      "cognitoPoolClaims" : {
-        
-        "sub": "$context.authorizer.claims.sub"
-      },
+  "requestContext": {
+    "requestId": "$context.requestId"
+  },
 
-      "requestContext": {
-        "requestId": "$context.requestId"
-      },
+  #set( $map = $context.authorizer )
+  "enhancedAuthContext": $loop,
 
-      #set( $map = $context.authorizer )
-      "enhancedAuthContext": $loop,
+  #set( $map = $input.params().querystring )
+  "query": $loop,
 
-      #set( $map = $input.params().querystring )
-      "query": $loop,
+  #set( $map = $input.params().path )
+  "pathParameters": $loop,
 
-      #set( $map = $input.params().path )
-      "pathParameters": $loop,
+  #set( $map = $context.identity )
+  "identity": $loop,
 
-      #set( $map = $context.identity )
-      "identity": $loop,
+  #set( $map = $stageVariables )
+  "stageVariables": $loop,
 
-      #set( $map = $stageVariables )
-      "stageVariables": $loop,
-
-      "requestPath": "$context.resourcePath"
-    }
-    EOF
+  "requestPath": "$context.resourcePath"
+}
+EOF
   }
 }
