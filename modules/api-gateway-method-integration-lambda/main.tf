7be3cb0012bcbcf9d55757f16e09a0e8348a4666
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2024-08-01 14:55:32 -0300

    fix: add response method

--- modules/api-gateway-method-integration-lambda/main.tf
+++ modules/api-gateway-method-integration-lambda/main.tf
@@ -4,9 +4,6 @@ resource "aws_api_gateway_method" "lambda" {
   http_method        = var.method
   authorization      = var.authorization != "NONE" ? "CUSTOM" : "NONE"
   authorizer_id      = var.authorization != "NONE" ? var.authorizer_id : null
-
-  request_parameters = { "method.request.path.proxy" = true }
-
 }
 
 resource "aws_lambda_permission" "lambda" {
@@ -27,7 +24,7 @@ resource "aws_api_gateway_integration" "lambda" {
   resource_id             = var.resource_id
   http_method             = var.method
   integration_http_method = var.method
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = var.uri_origin
   passthrough_behavior    = "WHEN_NO_MATCH"
 
