5bc8a570b01f2d1520f679e8338ad13bbf588381
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2024-09-20 09:11:35 -0300

    feat: add policy autoscale

--- modules/ecs-cluster-ec2/main.tf
+++ modules/ecs-cluster-ec2/main.tf
@@ -41,6 +41,22 @@ resource "aws_autoscaling_group" "ecs_cluster" {
   }
 }
 
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "${var.cluster_name}-scale-up"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.ecs_cluster.name
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "${var.cluster_name}-scale-down"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.ecs_cluster.name
+}
+
 resource "aws_ecs_cluster" "ecs-cluster-ec2" {
   name = var.cluster_name
 
