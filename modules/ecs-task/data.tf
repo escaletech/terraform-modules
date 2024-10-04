f663bdfcfee3dd02a8ef63fb86254e4f57551c1a
anselmojoao <169174630+anselmojoao@users.noreply.github.com>
2024-10-04 15:42:55 -0300

    fix retention logs (#240)
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

--- modules/ecs-task/data.tf
+++ modules/ecs-task/data.tf
@@ -5,3 +5,7 @@ data "aws_region" "current" {}
 data "aws_iam_policy" "ecs_task_cloudwatch_logs_policy" {
   name = "ecs_task_cloudwatch_logs_policy"
 }
+
+data "aws_cloudwatch_log_group" "logs" {
+  name = var.family
+}
