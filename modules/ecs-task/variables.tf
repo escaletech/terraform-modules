3971deee92018b91951f2e765a53b6cb4f447767
anselmojoao <169174630+anselmojoao@users.noreply.github.com>
2024-10-14 15:32:35 -0300

    fix var retention_in_days (#244)
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

--- modules/ecs-task/variables.tf
+++ modules/ecs-task/variables.tf
@@ -60,7 +60,7 @@ variable "arn_attach_additional_policy" {
   default     = []
 }
 
-variable "var.retention_in_days" {
+variable "retention_in_days" {
   description = "Quantidade de dias para retenção de logs."
   type        = number
   default     = 14
