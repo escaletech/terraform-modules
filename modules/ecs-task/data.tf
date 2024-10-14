1cb0ae9492003ccc79c7adbdcc562dd1857c54dd
anselmojoao <169174630+anselmojoao@users.noreply.github.com>
2024-10-14 16:38:10 -0300

    deleta data log group (#247)
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

--- modules/ecs-task/data.tf
+++ modules/ecs-task/data.tf
@@ -6,6 +6,6 @@ data "aws_iam_policy" "ecs_task_cloudwatch_logs_policy" {
   name = "ecs_task_cloudwatch_logs_policy"
 }
 
-data "aws_cloudwatch_log_group" "logs" {
-  name = var.family
-}
+# data "aws_cloudwatch_log_group" "logs" {
+#   name = var.family
+# }
