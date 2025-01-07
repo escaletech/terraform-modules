fef851bfd2148a9711234abe646dc11171f4ae5a
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2025-01-07 10:47:38 -0300

    fix policy document

--- modules/s3/data.tf
+++ modules/s3/data.tf
@@ -1,15 +1,7 @@
-# data "aws_iam_policy_document" "s3_secure_policy" {
-#   statement {
-#     actions   = var.s3_policy_document.statement[*].actions
-#     resources = var.s3_policy_document.statement[*].resources
-#     effect    = var.s3_policy_document.statement[*].effect
-#   }
-# }
-
 data "aws_iam_policy_document" "s3_secure_policy" {
   statement {
-    actions   = flatten([for s in var.s3_policy_document.statement : s.actions])
-    resources = flatten([for s in var.s3_policy_document.statement : s.resources])
-    effect    = join(",", [for s in var.s3_policy_document.statement : s.effect])
+    actions   = var.s3_policy_document.statement[0].actions
+    resources = var.s3_policy_document.statement[0].resources
+    effect    = var.s3_policy_document.statement[0].effect
   }
 }
