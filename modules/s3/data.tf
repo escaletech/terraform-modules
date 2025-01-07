05852e4ca48834e3c1cd02be086026f0740bbc9a
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2025-01-07 10:10:11 -0300

    fix policy document

--- modules/s3/data.tf
+++ modules/s3/data.tf
@@ -1,9 +1,7 @@
 data "aws_iam_policy_document" "s3_secure_policy" {
-  for_each = { for idx, statement in var.s3_policy_document.statement : idx => statement }
-
   statement {
-    actions   = each.value.actions
-    resources = each.value.resources
-    effect    = each.value.effect
+    actions   = var.s3_policy_document.statement[*].actions
+    resources = var.s3_policy_document.statement[*].resources
+    effect    = var.s3_policy_document.statement[*].effect
   }
 }
