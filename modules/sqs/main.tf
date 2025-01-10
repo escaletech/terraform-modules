c4fbae326333628bb8d3e81487a0a07c42f5a5e2
anselmojoao <169174630+anselmojoao@users.noreply.github.com>
2025-01-10 10:41:02 -0300

    add kms in sqs (#251)
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -4,15 +4,17 @@ locals {
 }
 
 resource "aws_sqs_queue" "sqs_queue" {
-  name                        = local.queue_name
-  delay_seconds               = var.delay_seconds
-  max_message_size            = var.max_message_size
-  message_retention_seconds   = var.message_retention_seconds
-  receive_wait_time_seconds   = var.receive_wait_time_seconds
-  tags                        = var.tags
-  fifo_queue                  = var.fifo_queue
-  content_based_deduplication = var.content_based_deduplication
-  redrive_policy              = var.redrive_policy
+  name                              = local.queue_name
+  delay_seconds                     = var.delay_seconds
+  max_message_size                  = var.max_message_size
+  message_retention_seconds         = var.message_retention_seconds
+  receive_wait_time_seconds         = var.receive_wait_time_seconds
+  tags                              = var.tags
+  fifo_queue                        = var.fifo_queue
+  content_based_deduplication       = var.content_based_deduplication
+  redrive_policy                    = var.redrive_policy
+  kms_master_key_id                 = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
 }
 
 resource "aws_sqs_queue_policy" "sqs_queue" {
