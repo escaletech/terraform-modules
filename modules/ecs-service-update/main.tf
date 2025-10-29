6c49a6031ea5311c86bc7593aa1ac2609b689e1d
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2025-10-29 11:18:28 -0300

    add ports in ecs service

--- modules/ecs-service-update/main.tf
+++ modules/ecs-service-update/main.tf
@@ -39,12 +39,20 @@ resource "aws_ecs_service" "ecs_service_update" {
   }
 
   dynamic "load_balancer" {
-    for_each = var.target_group_arn != null ? [1] : []
+    for_each = var.load_balancers != null && length(var.load_balancers) > 0 ? var.load_balancers : (
+      var.target_group_arn != null ? [
+        {
+          container_name   = var.service_name
+          container_port   = var.container_port
+          target_group_arn = var.target_group_arn
+        }
+      ] : []
+    )
 
     content {
-      container_name   = var.service_name
-      container_port   = var.container_port
-      target_group_arn = var.target_group_arn
+      container_name   = lookup(load_balancer.value, "container_name", var.service_name)
+      container_port   = load_balancer.value.container_port
+      target_group_arn = load_balancer.value.target_group_arn
     }
   }
 
