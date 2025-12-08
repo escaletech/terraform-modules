21030546a4567f7000f3369234ce09abb8c0ad2b
Joao Anselmo <joaoanselmo@ip-10-212-135-57.ec2.internal>
2025-12-08 18:23:17 -0300

    add var to disable certificate

--- modules/cdn/certificate.tf
+++ modules/cdn/certificate.tf
@@ -3,7 +3,8 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  count = var.certificate_enable == true ? 1 : 0
+  for_each = var.certificate_enable ? { default = true } : {}
+
   domain_name       = var.certificate_name != null ? var.certificate_name : var.host
   validation_method = "DNS"
   tags              = var.tags
@@ -14,25 +15,25 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = var.certificate_enable == true ? 1 : 0
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+  for_each = var.certificate_enable ? {
+    for dvo in aws_acm_certificate.cert["default"].domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
     }
-  }
+  } : {}
 
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = var.ttl
+  zone_id         = data.aws_route53_zone.zone.zone_id
+  name            = each.value.name
+  type            = each.value.type
+  records         = [each.value.record]
+  ttl             = var.ttl
   allow_overwrite = var.allow_overwrite
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  count = var.certificate_enable == true ? 1 : 0
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  for_each = var.certificate_enable ? { default = true } : {}
+
+  certificate_arn         = aws_acm_certificate.cert["default"].arn
+  validation_record_fqdns = values(aws_route53_record.cert_validation)[*].fqdn
 }
