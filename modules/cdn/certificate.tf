becc440059225769b2689c89c0582d0effa05963
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2025-12-08 14:42:19 -0300

    add var to enable certificate

--- modules/cdn/certificate.tf
+++ modules/cdn/certificate.tf
@@ -3,6 +3,7 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_acm_certificate" "cert" {
+  count             = var.create_certificate ? 1 : 0
   domain_name       = var.host
   validation_method = "DNS"
   tags              = var.tags
@@ -13,13 +14,13 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+  for_each = var.create_certificate ? {
+    for dvo in aws_acm_certificate.cert[0].domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
     }
-  }
+  } : {}
 
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = each.value.name
@@ -30,6 +31,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
+  count                   = var.create_certificate ? 1 : 0
+  certificate_arn         = aws_acm_certificate.cert[0].arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
