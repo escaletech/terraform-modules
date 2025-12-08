f40ca9e22952fb5125f5229fc26d8a7a31210374
Joao Anselmo <joaoanselmo@ip-10-212-135-57.ec2.internal>
2025-12-08 18:16:39 -0300

    add var to disable certificate

--- modules/cdn/certificate.tf
+++ modules/cdn/certificate.tf
@@ -14,6 +14,7 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
+  count = var.certificate_enable == true ? 1 : 0
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -31,6 +32,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert" {
+  count = var.certificate_enable == true ? 1 : 0
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
