3e62a04531e3edf1f8fb7ff2f309893df100783a
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2026-01-16 15:28:35 -0300

    Add private zone VPC associations

--- modules/dnszone/variables.tf
+++ modules/dnszone/variables.tf
@@ -7,3 +7,28 @@ variable "tags" {
   description = "The FQND of the domain"
   type        = map(any)
 }
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the DNS zone will be created (deprecated; use vpc_associations)"
+  type        = map(any)
+}
+
+variable "private" {
+  description = "Whether the zone is private and should be associated with VPCs"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_associations" {
+  description = "List of VPC associations for the private zone (id + region)"
+  type = list(object({
+    vpc_id     = string
+    vpc_region = string
+  }))
+  default = []
+
+  validation {
+    condition     = (!var.private) || (length(var.vpc_associations) > 0)
+    error_message = "When private=true, at least one VPC association must be provided in vpc_associations."
+  }
+}
--- modules/dnszone/zone.tf
+++ modules/dnszone/zone.tf
@@ -1,4 +1,22 @@
 resource "aws_route53_zone" "dev" {
   name = var.dnszone
   tags = var.tags
+
+  dynamic "vpc" {
+    for_each = var.private && length(var.vpc_associations) > 0 ? [var.vpc_associations[0]] : []
+    content {
+      vpc_id     = vpc.value.vpc_id
+      vpc_region = vpc.value.vpc_region
+    }
+  }
+}
+
+resource "aws_route53_zone_association" "private_vpcs" {
+  for_each = var.private && length(var.vpc_associations) > 1 ? {
+    for idx, assoc in var.vpc_associations : idx => assoc if idx > 0
+  } : {}
+
+  zone_id    = aws_route53_zone.dev.zone_id
+  vpc_id     = each.value.vpc_id
+  vpc_region = each.value.vpc_region
 }
