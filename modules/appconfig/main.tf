a451cfa107cbcbf9e33e048b3278035d466d06dd
DouglasFrancoDS <douglasfranco.contato@hotmail.com>
2026-01-22 15:18:07 -0300

    feat: app config

---
+++ modules/appconfig/main.tf
@@ -0,0 +1,48 @@
+resource "aws_appconfig_application" "app" {
+  name        = var.app_name
+  description = var.app_description
+  tags        = var.tags
+}
+
+resource "aws_appconfig_environment" "env" {
+  name           = var.env_name
+  description    = var.env_description
+  application_id = aws_appconfig_application.app.id
+  tags           = var.tags
+}
+
+resource "aws_appconfig_configuration_profile" "profile" {
+  application_id = aws_appconfig_application.app.id
+  name           = var.profile_name
+  location_uri   = "hosted"
+  tags           = var.tags
+}
+
+resource "aws_appconfig_hosted_configuration_version" "config_version" {
+  application_id           = aws_appconfig_application.app.id
+  configuration_profile_id = aws_appconfig_configuration_profile.profile.id
+  content_type             = var.content_type
+  content                  = var.config_content
+  description              = var.config_description
+}
+
+resource "aws_appconfig_deployment_strategy" "strategy" {
+  name                           = "${var.app_name}-${var.deployment_strategy_name}"
+  description                    = var.deployment_strategy_description
+  deployment_duration_in_minutes = var.deployment_duration_in_minutes
+  final_bake_time_in_minutes     = var.final_bake_time_in_minutes
+  growth_factor                  = var.growth_factor
+  growth_type                    = var.growth_type
+  replicate_to                   = var.replicate_to
+  tags                           = var.tags
+}
+
+resource "aws_appconfig_deployment" "deployment" {
+  application_id           = aws_appconfig_application.app.id
+  environment_id           = aws_appconfig_environment.env.id
+  configuration_profile_id = aws_appconfig_configuration_profile.profile.id
+  configuration_version    = aws_appconfig_hosted_configuration_version.config_version.version_number
+  deployment_strategy_id   = aws_appconfig_deployment_strategy.strategy.id
+  description              = var.deployment_description
+  tags                     = var.tags
+}
---
+++ modules/appconfig/outputs.tf
@@ -0,0 +1,34 @@
+output "application_id" {
+  description = "The ID of the AppConfig application."
+  value       = aws_appconfig_application.app.id
+}
+
+output "application_arn" {
+  description = "The ARN of the AppConfig application."
+  value       = aws_appconfig_application.app.arn
+}
+
+output "environment_id" {
+  description = "The ID of the AppConfig environment."
+  value       = aws_appconfig_environment.env.id
+}
+
+output "environment_arn" {
+  description = "The ARN of the AppConfig environment."
+  value       = aws_appconfig_environment.env.arn
+}
+
+output "configuration_profile_id" {
+  description = "The ID of the AppConfig configuration profile."
+  value       = aws_appconfig_configuration_profile.profile.id
+}
+
+output "configuration_version" {
+  description = "The version number of the deployed configuration."
+  value       = aws_appconfig_hosted_configuration_version.config_version.version_number
+}
+
+output "deployment_strategy_id" {
+  description = "The ID of the deployment strategy."
+  value       = aws_appconfig_deployment_strategy.strategy.id
+}
