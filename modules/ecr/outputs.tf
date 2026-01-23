28b4464a2930d0eacc455c8fdc03bc5a7cd467e9
DouglasFrancoDS <douglasfranco.contato@hotmail.com>
2026-01-23 17:40:03 -0300

    chore: add create ecr

--- modules/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  description = "The ARN of the repository."
-  value       = aws_ecr_repository.this.arn
-}
-
-output "url" {
-  description = "The URL of the repository (fqdn)."
-  value       = aws_ecr_repository.this.repository_url
-}
