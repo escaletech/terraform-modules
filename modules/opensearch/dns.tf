cdce0ad5d7f94ddfd09ba0a2fc85d904fd401c92
Malcoln França <malcolnsf@gmail.com>
2026-01-26 17:37:01 -0300

    cit-1762:create module opensearh

--- modules/opensearch/dns.tf
+++ modules/opensearch/dns.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "opensearch-dns" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${var.name}.${var.domain_name}}"
+  name    = "${var.name}"
   type    = "CNAME"
   records = [aws_opensearch_domain.opensearch.endpoint]
   ttl     = "300"
