8ff3ec8f788fb5407f8cafb3938e2c62b068530a
malcolnsf <67751508+malcolnsf@users.noreply.github.com>
2026-02-09 16:42:25 -0300

    cit-1739:create module n8n and efs (#288)
    * cit-1739:create module n8n and efs
    
    * fix vars
    
    * fix:variables efs
    
    * fix:description of sg
    
    * chore:change cpu and memory
    
    ---------
    
    Co-authored-by: Joao Anselmo <joaoanselmo@MBPMIB009575.local>

---
+++ modules/efs/variables.tf
@@ -0,0 +1,62 @@
+variable "service_name" {
+  description = "Nome do serviço/cliente (ex.: n8n-cliente-abc, n8n-staging)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID da VPC onde criar o EFS"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnets privadas para criar Mount Targets (mínimo 1, ideal 2+)"
+  type        = list(string)
+}
+
+variable "ecs_task_security_group_ids" {
+  description = "Lista de Security Groups das tasks ECS que vão acessar o EFS"
+  type        = list(string)
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags para todos os recursos"
+  type        = map(string)
+  default     = {}
+}
+
+variable "performance_mode" {
+  type    = string
+  default = "generalPurpose"
+}
+
+variable "throughput_mode" {
+  type    = string
+  default = "bursting"
+}
+
+variable "transition_to_ia" {
+  type    = string
+  default = "AFTER_30_DAYS"
+}
+
+variable "uid" {
+  type    = number
+  default = 1000
+}
+
+variable "gid" {
+  type    = number
+  default = 1000
+}
+
+variable "root_permissions" {
+  type    = string
+  default = "0755"
+}
+
+variable "efs_mount_path" {
+  description = "Caminho onde o EFS será montado dentro do container (ex.: /home/node)"
+  type        = string
+  default     = "/home/node"
+}
