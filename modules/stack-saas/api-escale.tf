b7090336ed1285cb0d71f345bec7e39a50c9968d
Joao Anselmo <joaoanselmo@ip-10-212-135-72.ec2.internal>
2026-02-11 11:58:58 -0300

    fix api data

--- modules/stack-saas/api-escale.tf
+++ modules/stack-saas/api-escale.tf
@@ -1,14 +1,14 @@
 
 resource "aws_api_gateway_resource" "api_evolution" {
   count       = var.enable_api_gateway ? 1 : 0
-  rest_api_id = data.aws_api_gateway_rest_api.api-escale.id
-  parent_id   = data.aws_api_gateway_rest_api.api-escale.root_resource_id
+  rest_api_id = data.aws_api_gateway_rest_api.api-escale[0].id
+  parent_id   = data.aws_api_gateway_rest_api.api-escale[0].root_resource_id
   path_part   = var.client_name
 }
 
 resource "aws_api_gateway_resource" "api_evolution_whatsapp" {
   count       = var.enable_api_gateway ? 1 : 0
-  rest_api_id = data.aws_api_gateway_rest_api.api-escale.id
+  rest_api_id = data.aws_api_gateway_rest_api.api-escale[0].id
   parent_id   = aws_api_gateway_resource.api_evolution[0].id
   path_part   = "whatsapp"
 
@@ -17,7 +17,7 @@ resource "aws_api_gateway_resource" "api_evolution_whatsapp" {
 
 resource "aws_api_gateway_resource" "api_evolution_meta" {
   count       = var.enable_api_gateway ? 1 : 0
-  rest_api_id = data.aws_api_gateway_rest_api.api-escale.id
+  rest_api_id = data.aws_api_gateway_rest_api.api-escale[0].id
   parent_id   = aws_api_gateway_resource.api_evolution_whatsapp[0].id
   path_part   = "meta"
 
@@ -26,7 +26,7 @@ resource "aws_api_gateway_resource" "api_evolution_meta" {
 
 resource "aws_api_gateway_method" "api_evolution_meta_any" {
   count         = var.enable_api_gateway ? 1 : 0
-  rest_api_id   = data.aws_api_gateway_rest_api.api-escale.id
+  rest_api_id   = data.aws_api_gateway_rest_api.api-escale[0].id
   resource_id   = aws_api_gateway_resource.api_evolution_meta[0].id
   http_method   = "ANY"
   authorization = "NONE"
@@ -36,14 +36,14 @@ resource "aws_api_gateway_method" "api_evolution_meta_any" {
 
 resource "aws_api_gateway_integration" "api_evolution_meta_any" {
   count                   = var.enable_api_gateway ? 1 : 0
-  rest_api_id             = data.aws_api_gateway_rest_api.api-escale.id
+  rest_api_id             = data.aws_api_gateway_rest_api.api-escale[0].id
   resource_id             = aws_api_gateway_resource.api_evolution_meta[0].id
   http_method             = aws_api_gateway_method.api_evolution_meta_any[0].http_method
   type                    = "HTTP_PROXY"
   passthrough_behavior    = "WHEN_NO_MATCH"
   integration_http_method = "ANY"
   connection_type         = "VPC_LINK"
-  connection_id           = data.aws_api_gateway_vpc_link.vpc_link_ecs_apps.id
+  connection_id           = data.aws_api_gateway_vpc_link.vpc_link_ecs_apps[0].id
   uri                     = "https://${var.client_name}-evolution.saas.escale.ai/webhook/meta"
 
   depends_on = [aws_api_gateway_method.api_evolution_meta_any]
@@ -54,7 +54,7 @@ resource "aws_api_gateway_integration" "api_evolution_meta_any" {
 
 resource "aws_api_gateway_domain_name" "custom_domain_name" {
   count           = var.enable_api_gateway ? 1 : 0
-  certificate_arn = data.aws_acm_certificate.xclapi.arn
+  certificate_arn = data.aws_acm_certificate.xclapi[0].arn
   domain_name     = "${var.client_name}.${var.api_gateway_zone_name}"
 }
 
@@ -62,7 +62,7 @@ resource "aws_route53_record" "domain_name" {
   count   = var.enable_api_gateway ? 1 : 0
   name    = aws_api_gateway_domain_name.custom_domain_name[0].domain_name
   type    = "A"
-  zone_id = data.aws_route53_zone.xclapi.zone_id
+  zone_id = data.aws_route53_zone.xclapi[0].zone_id
 
   alias {
     evaluate_target_health = true
@@ -73,7 +73,7 @@ resource "aws_route53_record" "domain_name" {
 
 resource "aws_api_gateway_base_path_mapping" "evolution" {
   count       = var.enable_api_gateway ? 1 : 0
-  api_id      = data.aws_api_gateway_rest_api.api-escale.id
+  api_id      = data.aws_api_gateway_rest_api.api-escale[0].id
   stage_name  = "production"
   domain_name = aws_api_gateway_domain_name.custom_domain_name[0].domain_name
 
