63fa23dd30ea4a7ede331b33c6cf888522a1accc
Joao Anselmo <joaoanselmo@MBPMIB009575.local>
2026-02-11 10:40:59 -0300

    fix api gateway evolution

--- modules/stack-saas/data.tf
+++ modules/stack-saas/data.tf
@@ -2,19 +2,23 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_api_gateway_rest_api" "api-escale" {
-  name = var.api_gateway_name
+  count = var.enable_api_gateway ? 1 : 0
+  name  = var.api_gateway_name
 }
 
 data "aws_api_gateway_vpc_link" "vpc_link_ecs_apps" {
-  name = var.api_gateway_vpc_link_name
+  count = var.enable_api_gateway ? 1 : 0
+  name  = var.api_gateway_vpc_link_name
 }
 
 data "aws_acm_certificate" "xclapi" {
+  count       = var.enable_api_gateway ? 1 : 0
   domain      = var.api_gateway_certificate_domain
   types       = ["AMAZON_ISSUED"]
   most_recent = true
 }
 
 data "aws_route53_zone" "xclapi" {
-  name = var.api_gateway_zone_name
+  count = var.enable_api_gateway ? 1 : 0
+  name  = var.api_gateway_zone_name
 }
